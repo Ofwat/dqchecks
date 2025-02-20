931fcc1a6db6a4c81fa4d0c79c24e2364be06714
coau <79207858+coau@users.noreply.github.com>
2025-02-20 09:55:09 +0000

    pylinting

--- dqchecks/panacea.py
+++ dqchecks/panacea.py
@@ -4,7 +4,30 @@ Panacea code
 Function used to do initial validation of the Excel files
 """
 
-import pandas as pd
+# import pandas as pd
 
 # Validate that the excel workbook has the correct set of Tabs.
 # compare if all excel sheets are present again comparison against template
+# def validate_tabs_between_spreadsheets(spreadsheet1, spreadsheet2, verbose = False):
+#     pass
+
+# # check_structure  compare if all excel tables have the same structure
+# # (number of columns and rows are the same compared to the template)
+# def check_sheet_structure(df1, df2):
+#     # Check if the number of rows and columns are the same
+#     if df1.shape != df2.shape:
+#         return False, "Different number of rows/columns"
+
+#     # Optionally check if the columns are the same (both name and order)
+#     if not df1.columns.equals(df2.columns):
+#         return False, "Columns are different"
+
+#     # Optionally check if the data types of columns are the same
+#     if not df1.dtypes.equals(df2.dtypes):
+#         return False, "Different data types for corresponding columns"
+
+#     # If all checks pass, the structure is the same
+#     return True, "Spreadsheets have the same structure"
+
+if __name__ == "__main__":
+    pass
