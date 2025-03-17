029cdab0fd9e8b73b97826a2cb1331b05a34fc79
coau <79207858+coau@users.noreply.github.com>
2025-03-17 11:08:20 +0000

    bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ofwat-dqchecks",
-    version="0.0.20",
+    version="0.0.22",
     packages=find_packages(),
     install_requires=[
         "pandas",
