1d17303b2475855ee6f8cda2e5eb942084e48372
coau <79207858+coau@users.noreply.github.com>
2025-04-11 15:25:38 +0100

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ofwat-dqchecks",
-    version="0.0.49",
+    version="0.0.50",
     packages=find_packages(),
     install_requires=[
         "pandas",
