b1aa6e8a61f17adfd609151976c5d8d24ff3c81d
coau <79207858+coau@users.noreply.github.com>
2025-09-16 15:14:13 +0100

    bump

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name="ofwat-dqchecks",
-    version="0.0.77",
+    version="0.0.78",
     author="Ofwat",
     description='Excel validations',
     long_description=long_description,
