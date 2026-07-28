d65481252c5197e80cb3c217bb3c21c144b5a7c8
BilalFawaz00 <Bilal.Fawaz@ofwat.gov.uk>
2026-07-28 15:02:37 +0100

    fixed PC_CD to PC_cd

--- dqchecks/qa.py
+++ dqchecks/qa.py
@@ -327,7 +327,7 @@ APR_OUTCOMES_KEY_COLS: list[str] = APR_BASE_KEY_COLS + [
     "Price_Index_Coverage_Cd",
     "Nze_scheme_Cd",
     "Ghg_Scope_Cd",
-    "PC_Cd",
+    "PC_cd",
     "Region_Cd",
     "WINEP_SO_Scheme_Cd",
 ]
@@ -341,7 +341,7 @@ APR_CHARGES_KEY_COLS: list[str] = APR_BASE_KEY_COLS + [
     "Price_Index_Coverage_Cd",
     "Nze_scheme_Cd",
     "Ghg_Scope_Cd",
-    "PC_Cd",
+    "PC_cd",
     "Region_Cd",
     "WINEP_SO_Scheme_Cd",
 ]
@@ -365,7 +365,7 @@ APR_INNOVATION_KEY_COLS: list[str] = APR_BASE_KEY_COLS + [
 
 APR_CUSTOMER_POLICY_KEY_COLS: list[str] = APR_BASE_KEY_COLS + [
     "Business_Unit_Cd",
-    "PC_Cd",
+    "PC_cd",
     "Region_Cd",
 ]
 
